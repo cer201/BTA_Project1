0f3c81ddc4564f425dd4bda79deaf369660ef3cb
Chris Cervantes <cervanteschris201@gmail.com>
2024-03-12 17:16:58 +0100

    fix write_to_history

--- CurrencyExchange.py
+++ CurrencyExchange.py
@@ -13,7 +13,7 @@ class CurrencyExchange:
     def write_to_history(self, hist_dict):
         current_hist_data = self.file_manager.read_json(self.hist_file_path)
         current_hist_data.append(hist_dict)
-        self.file_manager.write_json(self.hist_file_path, current_hist_data)
+        self.file_manager.write_json(current_hist_data, self.hist_file_path)
 
     def get_exchange_rates(self):
         try:
